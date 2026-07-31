6200e3ebe1c93ae14cdb414e59954acc2f273e42
Le Chi Nguyen <lechinguyen09@gmail.com>
2026-07-31 15:33:44 +0700

    fix

--- packages/ui/eslint.config.mjs
+++ packages/ui/eslint.config.mjs
@@ -1,4 +1,4 @@
-import react from '@printsel/eslint-config/react';
+import react from '@onosfactory/eslint-config/react';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config({ ignores: ['dist/**'] }, ...react);
